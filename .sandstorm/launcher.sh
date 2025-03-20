ceb27615d1f2537f1061eab9572f99595c7dccc2
Jacob Weisz <inbox@jacobweisz.com>
2025-03-20 00:09:40 -0500

    Bookworm box update

--- .sandstorm/launcher.sh
+++ .sandstorm/launcher.sh
@@ -21,10 +21,18 @@ mkdir -p /var/log/nginx
 rm -rf /var/run
 mkdir -p /var/run/php
 
+# Rotate log files larger than 512K
+log_files="$(find /var/log -type f -name '*.log')"
+for f in $log_files; do
+    if [ $(du -b "$f" | awk '{print $1}') -ge $((512 * 1024)) ] ; then
+        mv $f $f.1
+    fi
+done
+
 # Spawn php
-/usr/sbin/php-fpm7.4 --nodaemonize --fpm-config /etc/php/7.4/fpm/php-fpm.conf &
+/usr/sbin/php-fpm8.2 --nodaemonize --fpm-config /etc/php/8.2/fpm/php-fpm.conf &
 # Wait until php has bound its socket, indicating readiness
-wait_for php-fpm7.4 /var/run/php/php7.4-fpm.sock
+wait_for php-fpm8.2 /var/run/php/php8.2-fpm.sock
 
 # Start nginx.
 /usr/sbin/nginx -c /opt/app/.sandstorm/service-config/nginx.conf -g "daemon off;"
